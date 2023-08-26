6cedf283c3d6bcb6372effe0d6436fb39f7f004c
Reem-degais <reem.degais.33@gmail.com>
2023-08-25 17:09:26 -0700

    -

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
-#Using Puppet, create a file in /tmp
+# creates a file in /tmp
 
 file { '/tmp/school':
-  mode => '0744',
-  group => 'www-data',
-  owner => 'www-data',
-  content => 'I love Puppet'
+  content =>'I love Puppet',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
 }
