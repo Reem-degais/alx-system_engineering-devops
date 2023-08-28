ef2578dce2a82de0d6e1020ae0ce0de03a790f19
Reem-degais <reem.degais.33@gmail.com>
2023-08-27 17:18:07 -0700

    -

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,13 @@
+#configuration file for the local SSH client to
+#connect to a server without typing a password
+
+file_line { 'Turn off passwd auth':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => 'PasswordAuthentication no',
+}
+file_line { 'Declare identity file':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => 'IdentityFile ~/.ssh/school',
+}
