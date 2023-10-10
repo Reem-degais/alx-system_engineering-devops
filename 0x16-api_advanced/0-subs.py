d80a628d62845bc324c22fd725d5309bb302c8f0
Reem-degais <122857220+Reem-degais@users.noreply.github.com>
2023-10-11 02:47:22 +0300

    Create 0-subs.py

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" queries the Reddit API and returns the number of subscribers"""
+
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """Returns the total number of subscribers
+    for a given subreddit"""
+    url = 'https://www.reddit.com'
+    api_uri = '{}/r/{}/about.json'.format(url, subreddit)
+
+    
+    user_agent = {'User-Agent': 'Python/requests'}
+    res = requests.get(api_uri, headers=user_agent,
+                       allow_redirects=False)
+
+  
+    if res.status_code in [302, 404]:
+        return 0
+
+    
+    return res.json().get('data').get('subscribers')
