419174df9d3125bafbf371377d333dfd0bf18db2
Reem-degais <122857220+Reem-degais@users.noreply.github.com>
2023-10-19 17:04:32 +0300

    Update 0-the_sky_is_the_limit_not.pp

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,11 +1,8 @@
-#increase the amount of request.
-
-exec { 'fix--for--nginx':
-  command => '/bin/sed -i "s/15/4096/" /etc/default/nginx.dpkg-dist',
-  path => '/usr/local/bin/:/bin/',
+#0-the_sky_is_the_limit_not.pp set open file limit higher
+exec { 'set limit to 2000':
+  path    => '/bin',
+  command => "sed -i 's/15/2000/' /etc/default/nginx"
 }
-
-exec { 'nginx-restart':
-  command => '/etc/init.d/nginx restart',
-  path => '/etc/init.d/',
+exec { 'reboot nginx':
+  command => '/usr/sbin/service nginx restart'
 }
